0ce1ceaac18879d16c0cb27157df139ef3d212eb
Monika Palova <44198558+MonikaPalova@users.noreply.github.com>
2020-11-15 11:50:57 +0200

    task 2.3 format changes

--- Basic JavaScript/2.js
+++ Basic JavaScript/2.js
@@ -25,17 +25,27 @@ array1.forEach((item) => {
 //2.4-helpers
 function getDayName(a) {
   var weekdays = new Array(
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    "неделя", "понеделник", "вторник", "сряда", "четвъртък", "петък", "събота"
   );
   return weekdays[a];
+}
+
+function formatDigit(a) {
+  return a >= 10 ? a : "0" + a;
+}
+
+function formatDate(date) {
+  const DATE_DIVISOR = ".";
+  const TIME_DIVISOR = ":";
+  return "Дата: " + formatDigit(date.getDate()) + DATE_DIVISOR + formatDigit(date.getMonth() + 1) + DATE_DIVISOR + date.getFullYear() +
+    ", час: " + formatDigit(date.getHours()) + TIME_DIVISOR + formatDigit(date.getMinutes()) + TIME_DIVISOR + formatDigit(date.getSeconds());
 }
 
 function combine(date, arr) {
   const SEPARATOR = ", ";
   var dayNumber = arr[1];
   var numberOfDays = arr[0];
-
-  return date.toString() + SEPARATOR + getDayName(dayNumber) + SEPARATOR + numberOfDays + " days";
+  return formatDate(new Date(date)) + SEPARATOR + getDayName(dayNumber) + SEPARATOR + numberOfDays + " дни";
 }
 //2.4
 var array3 = new Array(2);
